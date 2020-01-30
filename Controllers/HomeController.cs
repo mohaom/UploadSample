b2d6502ed887fe939fc82be0d670fa5ea3d8270c
Mohammed Omar <mohaom@microsoft.com>
2020-01-30 13:38:56 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UploadFileSampler.Models;
+
+namespace UploadFileSampler.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string UploadFolder = $"{_hostingEnvironment.WebRootPath}\\Uploads";
+            if (Request.Method == "POST")
+                foreach (var upload in Request.Form.Files)
+                {
+                    System.IO.Directory.CreateDirectory(UploadFolder);
+                    using (var fileStream = new FileStream($"{UploadFolder}\\{upload.FileName}", FileMode.Create))
+                    {
+                        upload.CopyTo(fileStream);
+                    }
+
+                }
+            var files = Directory.GetFiles(UploadFolder).Select(x=>new FileInfo(x));
+            
+            return View(files);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Extension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UploadFileSampler
+{
+    public static class Extension
+    {
+        //public static bool HasFile(this HttpPostedFileBase file)
+        //{
+        //    return (file != null && file.ContentLength > 0) ? true : false;
+        //}
+    }
+}
